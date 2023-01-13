f5aa1e468afafea51d93346fe84fec855b07d40a
yjh0816 <2543kt@naver.com>
2023-01-13 13:52:41 +0900

    feat: Make basic structure

---
+++ pig-dice-game.py
@@ -0,0 +1,20 @@
+
+
+def select_level():
+    pass
+
+def easy_mode():
+    pass
+
+def roll_dice():
+    pass
+
+def e_computer_play():
+    pass
+
+def user_play():
+    pass
+
+if __name__ == '__main__':
+    print("start")
+    select_level()
