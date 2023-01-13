7dfeec4cf83df117a712de2da46d8f365d7dadbc
JakeUGT <swc03218@gmail.com>
2023-01-13 15:24:28 +0900

    feat: updated 2nd time roll prob%

--- pig-dice-game.py
+++ pig-dice-game.py
@@ -13,7 +13,44 @@ def roll_dice():
     return roll_dice
 
 def e_computer_play():
-    pass
+  cpu = 1
+  cpu_prob = [1]
+  final_value = 0
+  turn_value = 0
+  dice_value = 0
+
+  while cpu <3:
+    print("CPU's turn")
+    dice_value = roll_dice()
+    turn_value += dice_value
+    print("CPU rolled", dice_value)
+    cpu += 1
+    if dice_value == 1:
+      print("Turn score reset to 0")
+      print(turn_value)
+      print("***********************")
+      print("Player's turn")
+      return turn_value
+      break
+    else:
+      cpu_prob.append (2)
+      choice = random.choice(cpu_prob)
+      if choice == 1:
+        print("CPU chose to roll again")
+        dice_value = roll_dice()
+        turn_value += dice_value
+        print("CPU rolled", dice_value)
+        print("CPU's final value is ", turn_value)
+        print("***********************")
+        print("Player's turn")
+        break
+      else:
+        print("CPU chose to hold")
+        print("CPU's final value is ", turn_value)
+        print("***********************")
+        print("Player's turn")
+        break
+  return turn_value
 
 def user_play():
     is_play = True
