d5e7dd9074cb137c4809a36c1bd3744afa118317
yjh0816 <2543kt@naver.com>
2023-01-13 15:22:43 +0900

    feat: Separate roll and bank sequence from user play

--- pig-dice-game.py
+++ pig-dice-game.py
@@ -14,32 +14,36 @@ def roll_dice():
 
 def e_computer_play():
     pass
+def do_br(point_box: int):
+    is_play = True
+    print(f"point box: {point_box}")
+    rb = input("R or B: ")
+    dice_value = roll_dice()
 
-def user_play():
+    if rb == 'b':
+        # if select bank, get out of while
+        is_play = False
+    elif rb == 'r':
+    # if first dice is one, game end 
+        if dice_value == 1:
+            print("dice value: 1")
+            # user get lose point
+            point_box = 0
+            is_play = False
+        else: 
+            print(f"dice value: {dice_value}")
+            point_box += dice_value
+
+    return dice_value, point_box, is_play
+
+def user_play()->int:
     is_play = True
     point_box = 0
     dice_value = 0
 
     while is_play == True:
         
-        print(f"point box: {point_box}")
-        rb = input("R or B: ")
-        dice_value = roll_dice()
-
-        if rb == 'b':
-            # if select bank, get out of while
-            is_play = False
-        elif rb == 'r':
-        # if first dice is one, game end 
-            if dice_value == 1:
-                print("dice value: 1")
-                # user get lose point
-                point_box = 0
-                is_play = False
-            else: 
-                print(f"dice value: {dice_value}")
-                point_box += dice_value
-                continue
+        dice_value, point_box, is_play = do_br(point_box)
     print(f"point box: {point_box}")
     # in the end, return value is point
     return point_box
