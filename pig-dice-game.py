1706d62c2b9f4241008ce72a9dd0ae09f1ef85b7
yjh0816 <2543kt@naver.com>
2023-01-13 14:23:07 +0900

    feat: Create user play and Modify random dice value

--- pig-dice-game.py
+++ pig-dice-game.py
@@ -9,18 +9,44 @@ def easy_mode():
     pass
 
 def roll_dice():
-    roll_dice = randint(1,6+1)
+    roll_dice = randint(1,6)
     return roll_dice
 
 def e_computer_play():
     pass
 
 def user_play():
-    pass
+    is_play = True
+    point_box = 0
+    dice_value = 0
+
+    while is_play == True:
+        
+        print(f"point box: {point_box}")
+        dice_value = roll_dice()
+        # if first dice is one, game end 
+        if dice_value == 1:
+            print("dice value: 1")
+            # user get lose point
+            point_box = 0
+            is_play = False
+        else:   
+            point_box += dice_value
+            print(f"dice value: {dice_value}")
+            rb = input("R or B: ")
+            if rb == 'b':
+                # if select bank, get out of while
+                is_play = False
+            elif rb == 'r':
+                continue
+    print(f"point box: {point_box}")
+    # in the end, return value is point
+    return point_box
 
 if __name__ == '__main__':
     print("start")
     select_level()
+    user_play()
 
 
 
